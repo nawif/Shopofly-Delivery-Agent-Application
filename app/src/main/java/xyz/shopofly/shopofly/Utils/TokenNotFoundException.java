9a0172b42b3a31569e55afb8764c684418b0ce41
nawif <nnawif@gmail.com>
2019-04-10 19:37:16 +0300

    added a custom exception token not found

---
+++ app/src/main/java/xyz/shopofly/shopofly/Utils/TokenNotFoundException.java
@@ -0,0 +1,7 @@
+package xyz.shopofly.shopofly.Utils;
+
+public class TokenNotFoundException extends Exception {
+    public TokenNotFoundException(String message) {
+        super(message);
+    }
+}
